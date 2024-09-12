2ab1bffe86739cd46f881e30276b75f7e0243043
AndresRamos <andfera201@gmail.com>
2024-09-11 20:27:21 -0500

    feat: show chessboard squares

--- src/main/java/com/andresramos/chessgamepracticejava/controllers/ChessGameManagerController.java
+++ src/main/java/com/andresramos/chessgamepracticejava/controllers/ChessGameManagerController.java
@@ -2,5 +2,11 @@ package com.andresramos.chessgamepracticejava.controllers;
 
 public class ChessGameManagerController {
 
+    public int sizeRowBoard() {
+        return 8;
+    }
 
+    public int sizeColumnBoard() {
+        return 8;
+    }
 }
--- src/main/java/com/andresramos/chessgamepracticejava/ui/views/ChessBoardView.java
+++ src/main/java/com/andresramos/chessgamepracticejava/ui/views/ChessBoardView.java
@@ -2,10 +2,28 @@ package com.andresramos.chessgamepracticejava.ui.views;
 
 import com.andresramos.chessgamepracticejava.controllers.ChessGameManagerController;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class ChessBoardView extends GridPane {
 
-    public ChessBoardView(ChessGameManagerController chessGameController) {
+    private final ChessGameManagerController chessGameManagerController;
 
+    public ChessBoardView(ChessGameManagerController chessGameController) {
+        this.chessGameManagerController = chessGameController;
+        createChessBoard();
+    }
+
+    private void createChessBoard() {
+        int sizeRowBoard = chessGameManagerController.sizeRowBoard();
+        int sizeColumnBoard = chessGameManagerController.sizeColumnBoard();
+        for (int row = 0; row < sizeRowBoard; row++) {
+            for (int column = 0; column < sizeColumnBoard; column++) {
+                Rectangle rectangle = new Rectangle(75, 75);
+                Color color = ((row + column) % 2 == 0) ? Color.BLACK : Color.WHITE;
+                rectangle.setFill(color);
+                add(rectangle, column, row);
+            }
+        }
     }
 }
